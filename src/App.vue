335b50719b4601365669dc6579272e49082b8804
Kuharenko <iamksergey@mail.ru>
2020-04-24 15:57:14 +0300

    3

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,56 @@
 <template>
-  <h1>Hello Vue</h1>
+  <v-app id="inspire">
+    <v-navigation-drawer
+            v-model="drawer"
+            app
+            temporary
+    >
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-contact-mail</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Contact</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app dark color="primary">
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"/>
+      <v-toolbar-title>Application</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn text>Link One</v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+
+
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
 
 export default {
   name: 'App',
+  props: {
+    source: String,
+  },
   data: () => ({
-    //
+    drawer: null,
   }),
 };
 </script>
