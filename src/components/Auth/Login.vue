66dc1f8fc2b68edb97a903d240c9d33c9c975db1
Kuharenko <iamksergey@mail.ru>
2020-04-29 08:49:10 +0300

    Создние страницы логина

--- src/components/Auth/Login.vue
+++ src/components/Auth/Login.vue
@@ -1,16 +1,96 @@
 <template>
-    <v-container>
-        <v-layout>
-            <v-flex row>
-                <h1>Login</h1>
-            </v-flex>
-        </v-layout>
+    <v-container
+            class="fill-height"
+            fluid
+    >
+        <v-row
+                align="center"
+                justify="center"
+        >
+            <v-col
+                    cols="12"
+                    sm="8"
+                    md="6"
+            >
+                <v-card class="elevation-12">
+                    <v-toolbar
+                            color="primary"
+                            dark
+                            flat
+                    >
+                        <v-toolbar-title>Login form</v-toolbar-title>
+                    </v-toolbar>
+                    <v-card-text>
+                        <v-form v-model="valid" ref="form" validation>
+                            <v-text-field
+                                    prepend-icon="person"
+                                    name="email"
+                                    label="Email"
+                                    type="email"
+                                    v-model="email"
+                                    :rules="emailRules"
+                            />
+
+                            <v-text-field
+                                    prepend-icon="lock"
+                                    id="password"
+                                    name="password"
+                                    label="Password"
+                                    type="password"
+                                    :counter="6"
+                                    v-model="password"
+                                    :rules="passwordRules"
+                            />
+                        </v-form>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer/>
+                        <v-btn
+                                color="primary"
+                                @click="onSubmit"
+                                :disabled="!valid"
+                        >
+                            Login
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+        </v-row>
     </v-container>
 </template>
 
 <script>
+
+
     export default {
-        name: "Login"
+        name: "Login",
+        data: () => {
+            return {
+                email: '',
+                password: '',
+                valid: false,
+                emailRules: [
+                    v => !!v || 'E-mail is required',
+                    v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+                ],
+                passwordRules: [
+                    v => !!v || 'Password is required',
+                    v => (v && v.length >= 6) || 'Password must be equal or more than 6 characters'
+                ]
+            }
+        },
+        methods: {
+            onSubmit () {
+                if (this.$refs.form.validate()) {
+                    const user = {
+                        email: this.email,
+                        password: this.password
+                    }
+
+                    console.log(user)
+                }
+            }
+        }
     }
 </script>
 
