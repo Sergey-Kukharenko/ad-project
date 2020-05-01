9f27dc694bc3a3b1431031ac276fb4bfaf8aa085
Kuharenko <iamksergey@mail.ru>
2020-05-01 08:48:24 +0300

    Home

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,9 @@
               @click.stop="drawer = !drawer"
               class="hidden-md-and-up"
       />
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-toolbar-title>
+        <router-link to="/" tag="span" class="pointer">Ad application</router-link>
+      </v-toolbar-title>
 
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
@@ -71,3 +73,9 @@ export default {
   }),
 };
 </script>
+
+<style scoped>
+  .pointer {
+    cursor: pointer;
+  }
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,66 @@
 <template>
-    <v-container>
-        <v-layout>
-            <v-flex row>
-                <h1>Home</h1>
+<div>
+    <v-container fluid>
+        <v-layout row>
+            <v-flex xs12>
+                <v-carousel>
+                    <v-carousel-item
+                            v-for="ad in ads"
+                            :key="ad.id"
+                            :src="ad.imageSrc"
+                    >
+                        <div class="car-link">
+                            <v-btn class="error" :to="'/ad/' + ad.id">{{ad.title}}</v-btn>
+                        </div>
+                    </v-carousel-item>
+                </v-carousel>
             </v-flex>
         </v-layout>
     </v-container>
+    <v-container grid-list-lg>
+        <v-layout row wrap>
+            <v-flex
+                    xs12
+                    sm6
+                    md4
+                    v-for="ad of ads"
+                    :key="ad.id"
+            >
+                <v-card>
+                    <v-img
+                            :src="ad.imageSrc"
+                            height="200px"
+                    >
+
+                    </v-img>
+
+                    <v-card-title>
+                        {{ad.title}}
+                    </v-card-title>
+
+                    <v-card-subtitle>
+                        {{ad.description}}
+                    </v-card-subtitle>
+
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn text :to="'/ad/' + ad.id">Open</v-btn>
+                        <v-btn
+                                raised
+                                color="white"
+                                text
+                                class="primary"
+                        >
+                            Buy
+                        </v-btn>
+
+                    </v-card-actions>
+
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</div>
 </template>
 
 <script>
@@ -13,12 +68,43 @@
         name: "Home",
         data() {
             return {
-
+                ads: [
+                    {
+                        title: 'First ad',
+                        description: 'Lorem ipsum dolor sit amet',
+                        promo: false,
+                        imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+                        id: '123'
+                    },
+                    {
+                        title: 'Second ad',
+                        description: 'Lorem ipsum dolor sit amet',
+                        promo: true,
+                        imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+                        id: '1234'
+                    },
+                    {
+                        title: 'Third ad',
+                        description: 'Lorem ipsum dolor sit amet',
+                        promo: true,
+                        imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+                        id: '12345'
+                    }
+                ]
             }
         }
     }
 </script>
 
 <style scoped>
-
+    .car-link {
+        position: absolute;
+        bottom: 50px;
+        left: 50%;
+        background: rgba(0, 0, 0, .5);
+        transform: translate(-50%, 0);
+        padding: 5px 15px;
+        border-top-right-radius: 5px;
+        border-top-left-radius: 5px;
+    }
 </style>
