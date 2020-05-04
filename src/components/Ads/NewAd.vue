bfdbc0b71db892726483aa70dd776ed60aec176c
Kuharenko <iamksergey@mail.ru>
2020-05-04 08:19:28 +0300

    NewAd

--- src/components/Ads/NewAd.vue
+++ src/components/Ads/NewAd.vue
@@ -1,8 +1,60 @@
 <template>
     <v-container>
         <v-layout>
-            <v-flex row>
-                <h1>NewAd</h1>
+            <v-flex xs12 sm6 offset-sm3>
+                <h1 class="text-secondary mb-3">NewAd</h1>
+                <v-form v-model="valid" ref="form" validation class="mb-3">
+                    <v-text-field
+                            name="title"
+                            label="Ad title"
+                            type="text"
+                            v-model="title"
+                            :rules="[v => !!v || 'Title is required']"
+                            required
+                    />
+
+                    <v-textarea
+                            label="Ad description"
+                            type="text"
+                            v-model="description"
+                            multi-line
+                            :rules="[v => !!v || 'Description is required']"
+                    ></v-textarea>
+                </v-form>
+                <v-layout row class="mb-3">
+                    <v-flex xs12>
+                        <v-btn class="warning">
+                            Upload
+                            <v-icon right dark>cloud_upload</v-icon>
+                        </v-btn>
+                    </v-flex>
+                </v-layout>
+                <v-layout row>
+                    <v-flex xs12>
+                        <img src="" height="100">
+                    </v-flex>
+                </v-layout>
+                <v-layout row>
+                    <v-flex xs12>
+                        <v-spacer></v-spacer>
+                        <v-btn
+                                :disabled="!valid"
+                                class="success"
+                                @click="createAd"
+                        >
+                            Create ad
+                        </v-btn>
+                    </v-flex>
+                </v-layout>
+                <v-layout row>
+                    <v-flex xs12>
+                        <v-switch
+                                label="Add to promo?"
+                                v-model="promo"
+                                color="primary"
+                        ></v-switch>
+                    </v-flex>
+                </v-layout>
             </v-flex>
         </v-layout>
     </v-container>
@@ -10,7 +62,28 @@
 
 <script>
     export default {
-        name: "NewAd"
+        name: "NewAd",
+        data() {
+            return {
+                title: '',
+                description: '',
+                promo: false,
+                valid: false
+            }
+        },
+        methods: {
+            createAd() {
+                if (this.$refs.form.validate()) {
+                    const ad = {
+                        title: this.title,
+                        description: this.description,
+                        promo: this.promo,
+                    }
+                console.log(ad);
+                }
+
+            }
+        }
     }
 </script>
 
