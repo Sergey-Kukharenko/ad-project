225bfebefc89996806b8501ed6b0e332caf98e1e
Kuharenko <iamksergey@mail.ru>
2020-05-05 08:27:22 +0300

    Страница объявления

--- src/components/Ads/Ad.vue
+++ src/components/Ads/Ad.vue
@@ -1,8 +1,22 @@
 <template>
     <v-container>
-        <v-layout>
-            <v-flex row>
-                <h1>Ad</h1>
+        <v-layout row>
+            <v-flex xs12>
+                <v-card>
+                    <v-img
+                        src="https://cdn.vuetifyjs.com/images/carousel/planet.jpg"
+                        height="300px"
+                    ></v-img>
+                    <v-card-text>
+                        <h1 class="text--primary">lorem</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, nostrum.</p>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn class="warning" text>Edit</v-btn>
+                        <v-btn class="success">Buy</v-btn>
+                    </v-card-actions>
+                </v-card>
             </v-flex>
         </v-layout>
     </v-container>
